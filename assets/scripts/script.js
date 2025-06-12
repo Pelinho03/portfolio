a29466f3826fc8580a01feda7b4823cce499819d
Paulo Guimarães <elmuchacho@outlook.pt>
2025-06-12 20:21:23 +0100

    menu hamburguer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -74,3 +74,17 @@ window.addEventListener("scroll", function (event) {
         botao.classList.add("visible");
     }
 });
+
+function menuShow() {
+    let menuMobile = document.querySelector(".mobile-menu");
+    let icon = document.querySelector(".icon i");
+    if (menuMobile.classList.contains("open")) {
+        menuMobile.classList.remove("open");
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    } else {
+        menuMobile.classList.add("open");
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+    }
+}
